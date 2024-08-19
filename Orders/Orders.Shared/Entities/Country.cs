3800a6c63f9c19efdea648aadbc8742407b17aa7
Julian Hurtado <jhurtadot01@outlook.com>
2024-08-19 19:38:01 -0400

    First data migration Countries

---
+++ Orders/Orders.Shared/Entities/Country.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Orders.Shared.Entities
+{
+    public class Country
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "país")]
+        [MaxLength(255, ErrorMessage = "El campo {0} no puede tener más de {1} caracteres.")]
+        [Required(ErrorMessage = "El campo {0} es requerido.")]
+        public required string Name { get; set; }
+    }
+}
